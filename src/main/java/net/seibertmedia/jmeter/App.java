f6d6a92b283d50a91fe813b5a3610abc5108a225
Tilman Potthof <tpotthof@seibert-media.net>
2018-06-12 21:14:32 +0200

    Add maven hello world

---
+++ src/main/java/net/seibertmedia/jmeter/App.java
@@ -0,0 +1,13 @@
+package net.seibertmedia.jmeter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
