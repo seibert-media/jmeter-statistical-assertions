4c413016be3c900c3d89753f2400ac53911b56b4
Tilman Potthof <tpotthof@seibert-media.net>
2018-06-13 10:48:38 +0200

    Parameterize jar version in run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@
 set -e
 
 atlas-mvn clean compile assembly:single
-java -jar target/jmeter-statistical-assertions-0.1.0-SNAPSHOT-jar-with-dependencies.jar report test-data/aggregate.csv
+
+jar_path=$(ls target/jmeter-statistical-assertions-*-jar-with-dependencies.jar)
+
+java -jar ${jar_path} report test-data/aggregate.csv
